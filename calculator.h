081fe779d34c22046cd0b650129818da8442c4eb
amberlow <56101050+amberlow@users.noreply.github.com>
2019-11-13 22:37:48 -0800

    Add files via upload

--- calculator.h
+++ calculator.h
@@ -1,29 +1,30 @@
 #include "stackADT.h"
+#include "queueADT.h"
 
 
 class Calculator //: public StackADT<T>
 {
 private:
-	std::string inFix, postFix;
+    std::string inFix, postFix;
 
 public:
-	Calculator() {};										// default constructor
-	//std::string inFixToPostfix(std::string str);
-	//std::string inFixToPrefix(std::string infix);
-	int checkPrecedence(char c);							 // determines PMMDAS precedence
-	bool isOperator(char c);								// determines if character is bool/char
-	std::string infixToPostfix(std::string infix);			// converts expression to postfix
-	std::string infixToPrefix(std::string infix);			 // converts expression to prefix
-	int evalPostFix(std::string expression);
-	int postCalculation(char operation, int operand1, int operand2);
-	bool isNum(char C);
+    Calculator() {};                                        // default constructor
+    //std::string inFixToPostfix(std::string str);
+    //std::string inFixToPrefix(std::string infix);
+    int checkPrecedence(char c);                             // determines PMMDAS precedence
+    bool isOperator(char c);                                // determines if character is bool/char
+    std::string infixToPostfix(std::string infix);            // converts expression to postfix
+    std::string infixToPrefix(std::string infix);             // converts expression to prefix
 
-	/*double evalPostFix(std::string postfix);
-	double calculate(double n1, double n2, char op);*/
+    double evalPostFix(std::string expression);
+    double postCalculation(char operation, double operand1, double operand2);
+    bool isNum(char C);
+    double evalPreFix(std::string prefix);
+    double evaluatePrefix(std::string expression);
+
 
 
 };
-
 
 //******************************************************************
 //              int checkPrecendence(char)
@@ -35,16 +36,16 @@ public:
 //*******************************************************************
 int Calculator::checkPrecedence(char c)
 {
-	if (c == '%' || c == ' ') // PMMDAS is module higher than */? idk lol
-		return 3;
-	else if (c == '*' || c == '/')
-		return 2;
-	else if (c == '+' || c == '-')
-		return 1;
-	else
-		return 0;
-
-
+    if(c == ' ')
+        return 4;
+    if (c == '%' ) // PMMDAS is module higher than */? idk lol
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return 0;
 }
 
 
@@ -56,7 +57,7 @@ int Calculator::checkPrecedence(char c)
 // operator or not. true = isoperator, false = digit/char
 //*******************************************************************
 bool Calculator::isOperator(char c) {
-	return (!isalpha(c) && !isdigit(c));
+    return (!isalpha(c) && !isdigit(c));
 }
 
 
@@ -70,54 +71,54 @@ bool Calculator::isOperator(char c) {
 //*******************************************************************
 std::string Calculator::infixToPostfix(std::string infix)
 {
-	infix = '(' + infix + ')';
-	int l = infix.size();
-	StackADT<char> char_stack;
-	std::string output;
+    infix = '(' + infix + ')';
+    int l = infix.size();
+    StackADT<char> char_stack;
+    std::string output;
 
 
 
-	for (int i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
 
-		// If the scanned character is an 
-		// operand, add it to output. 
-		if (isalpha(infix[i]) || isdigit(infix[i]))
-			output += infix[i];
+        // If the scanned character is an
+        // operand, add it to output.
+        if (isalpha(infix[i]) || isdigit(infix[i]))
+            output += infix[i];
 
-		// If the scanned character is an 
-		// ‘(‘, push it to the stack. 
-		else if (infix[i] == '(')
-			char_stack.push('(');
+        // If the scanned character is an
+        // â€˜(â€˜, push it to the stack.
+        else if (infix[i] == '(')
+            char_stack.push('(');
 
-		// If the scanned character is an 
-		// ‘)’, pop and output from the stack 
-		// until an ‘(‘ is encountered. 
-		else if (infix[i] == ')') {
+        // If the scanned character is an
+        // â€˜)â€™, pop and output from the stack
+        // until an â€˜(â€˜ is encountered.
+        else if (infix[i] == ')') {
 
-			while (char_stack.peek() != '(') {
-				output += char_stack.peek();
-				char_stack.pop();
-			}
+            while (char_stack.peek() != '(') {
+                output += char_stack.peek();
+                char_stack.pop();
+            }
 
-			// Remove '(' from the stack 
-			char_stack.pop();
-		}
+            // Remove '(' from the stack
+            char_stack.pop();
+        }
 
-		// Operator found 
-		else {
-			if (isOperator(char_stack.peek()))
-			{
-				while (checkPrecedence(infix[i]) <= checkPrecedence(char_stack.peek()))
-				{
-					output += char_stack.peek();
-					char_stack.pop();
-				}
-				// Push current Operator on stack 
-				char_stack.push(infix[i]);
-			}
-		}
-	}
-	return output;
+        // Operator found
+        else {
+            if (isOperator(char_stack.peek()))
+            {
+                while (checkPrecedence(infix[i]) <= checkPrecedence(char_stack.peek()))
+                {
+                    output += char_stack.peek();
+                    char_stack.pop();
+                }
+                // Push current Operator on stack
+                char_stack.push(infix[i]);
+            }
+        }
+    }
+    return output;
 }
 
 
@@ -130,166 +131,279 @@ std::string Calculator::infixToPostfix(std::string infix)
 //*******************************************************************
 std::string Calculator::infixToPrefix(std::string infix)
 {
-	// stack for operators. 
-	StackADT<char> operators;
+    // stack for operators.
+    StackADT<char> operators;
 
-	// stack for operands. 
-	StackADT<std::string> operands;
+    // stack for operands.
+    StackADT<std::string> operands;
 
-	for (int i = 0; i < infix.length(); i++) {
+    for (int i = 0; i < infix.length(); i++) {
 
-		// If current character is an 
-		// opening bracket, then 
-		// push into the operators stack. 
-		if (infix[i] == '(') {
-			if (infix[i] != ' ')
-				operators.push(infix[i]);
-		}
+        // If current character is an
+        // opening bracket, then
+        // push into the operators stack.
+        if (infix[i] == '(') {
+            if (infix[i] != ' ')
+                operators.push(infix[i]);
+        }
 
-		// If current character is a 
-		// closing bracket, then pop from 
-		// both stacks and push result 
-		// in operands stack until 
-		// matching opening bracket is 
-		// not found. 
-		else if (infix[i] == ')') {
-			while (!operators.isEmpty() &&
-				operators.peek() != '(') {
+        // If current character is a
+        // closing bracket, then pop from
+        // both stacks and push result
+        // in operands stack until
+        // matching opening bracket is
+        // not found.
+        else if (infix[i] == ')') {
+            while (!operators.isEmpty() &&
+                operators.peek() != '(') {
 
-				// operand 1 
-				std::string op1 = operands.peek();
-				operands.pop();
+                // operand 1
+                std::string op1 = operands.peek();
+                operands.pop();
 
-				// operand 2 
-				std::string op2 = operands.peek();
-				operands.pop();
+                // operand 2
+                std::string op2 = operands.peek();
+                operands.pop();
 
-				// operator 
-				char op = operators.peek();
-				operators.pop();
+                // operator
+                char op = operators.peek();
+                operators.pop();
 
-				// Add operands and operator 
-				// in form operator + 
-				// operand1 + operand2. 
-				std::string tmp = op + op2 + op1;
-				operands.push(tmp);
-			}
+                // Add operands and operator
+                // in form operator +
+                // operand1 + operand2.
+                std::string tmp = op + op2 + op1;
+                operands.push(tmp);
+            }
 
-			// Pop opening bracket from 
-			// stack. 
-			operators.pop();
-		}
+            // Pop opening bracket from
+            // stack.
+            operators.pop();
+        }
 
-		// If current character is an 
-		// operand then push it into 
-		// operands stack. 
-		else if (!isOperator(infix[i])) {
-			operands.push(std::string(1, infix[i]));
-		}
+        // If current character is an
+        // operand then push it into
+        // operands stack.
+        else if (!isOperator(infix[i])) {
+            operands.push(std::string(1, infix[i]));
+        }
 
-		// If current character is an 
-		// operator, then push it into 
-		// operators stack after popping 
-		// high priority operators from 
-		// operators stack and pushing 
-		// result in operands stack. 
-		else {
-			while (operators.isEmpty() &&
-				checkPrecedence(infix[i]) <=
-				checkPrecedence(operators.peek())) {
+        // If current character is an
+        // operator, then push it into
+        // operators stack after popping
+        // high priority operators from
+        // operators stack and pushing
+        // result in operands stack.
+        else {
+            while (!operators.isEmpty() &&
+                checkPrecedence(infix[i]) <=
+                checkPrecedence(operators.peek())) {
 
-				std::string op1 = operands.peek();
-				operands.pop();
+                std::string op1 = operands.peek();
+                operands.pop();
 
-				std::string op2 = operands.peek();
-				operands.pop();
+                std::string op2 = operands.peek();
+                operands.pop();
 
-				char op = operators.peek();
-				operators.pop();
+                char op = operators.peek();
+                operators.pop();
 
-				std::string tmp = op + op2 + op1;
-				operands.push(tmp);
-			}
+                std::string tmp = op + op2 + op1;
+                operands.push(tmp);
+            }
 
-			operators.push(infix[i]);
-		}
-	}
+            operators.push(infix[i]);
+        }
+    }
 
-	// Pop operators from operators stack 
-	// until it is empty and add result 
-	// of each pop operation in 
-	// operands stack. 
-	while (operators.isEmpty()) {
-		std::string op1 = operands.peek();
-		operands.pop();
+    // Pop operators from operators stack
+    // until it is empty and add result
+    // of each pop operation in
+    // operands stack.
+    while (operators.isEmpty()) {
+        std::string op1 = operands.peek();
+        operands.pop();
 
-		std::string op2 = operands.peek();
-		operands.pop();
+        std::string op2 = operands.peek();
+        operands.pop();
 
-		char op = operators.peek();
-		operators.pop();
+        char op = operators.peek();
+        operators.pop();
 
-		std::string tmp = op + op2 + op1;
-		operands.push(tmp);
-	}
+        std::string tmp = op + op2 + op1;
+        operands.push(tmp);
+    }
 
-	// Final prefix expression is 
-	// present in operands stack. 
-	return operands.peek();
+    // Final prefix expression is
+    // present in operands stack.
+    return operands.peek();
 }
 
-
-
-
-// Evaluates postfix expression
-int Calculator::evalPostFix(std::string expression)
+//******************************************************************
+//                 double evalPostFix(std::String)
+// pre: receives the user input expression in postfix notation
+// pos: returns a double value representing the result
+// This function takes in the expression and evaluates it. The result
+// is then returned back to the main
+//*******************************************************************
+double Calculator::evalPostFix(std::string expression)
 {
-	// StackADT from previous LAB
-	StackADT<int> S;
+    // StackADT from previous LAB
+    StackADT<double> S;
 
-	for (int i = 0; i < expression.length(); i++) {
+    for (int i = 0; i < expression.length(); i++) {
 
-		// Skip whitespace char
-		if (expression[i] == ' ' || expression[i] == ',') continue;
+        // Skip whitespace char
+        if (expression[i] == ' ' || expression[i] == ',') continue;
 
-		// If character is operator, pop two elements from stack, perform operation and push the result back. 
-		else if (isOperator(expression[i])) {
-			int operand2 = S.peek(); S.pop();
-			int operand1 = S.peek(); S.pop();
-			int result = postCalculation(expression[i], operand1, operand2);
-			S.push(result);
-		}
-		else if (isNum(expression[i])) {
-			int operand = 0;
-			while (i < expression.length() && isNum(expression[i])) {
-				operand = (operand * 10) + (expression[i] - '0');
-				i++;
-			}
-			i--;
+        // If character is operator, pop two elements from stack, perform operation and push the result back.
+        else if (isOperator(expression[i])) {
+            double operand2 = S.peek(); S.pop();
+            double operand1 = S.peek(); S.pop();
+            double result = postCalculation(expression[i], operand1, operand2);
+            S.push(result);
+        }
+        else if (isNum(expression[i])) {
+            int operand = 0;
+            while (i < expression.length() && isNum(expression[i])) {
+                operand = (operand * 10) + (expression[i] - '0');
+                i++;
+            }
+            i--;
 
-			S.push(operand);
-		}
-	}
+            S.push(operand);
+        }
+    }
 
-	return S.peek();	// Returns the result of postfix evaluation
+    return S.peek();    // Returns the result of postfix evaluation
 }
 
-// Function to perform an operation and return output. 
-int Calculator::postCalculation(char operation, int operand1, int operand2)
+
+//******************************************************************
+//         int postCalculation(char, int, int)
+// pre: recieves a char operation, and two integers for operands
+// pos: returns a double value
+// this function take in two operands along with the operator calculates
+// the value. Returns that value back to be entered back to stack
+//*******************************************************************
+double Calculator::postCalculation(char operation, double operand1, double operand2)
 {
-	if (operation == '+') return operand1 + operand2;
-	else if (operation == '-') return operand1 - operand2;
-	else if (operation == '*') return operand1 * operand2;
-	else if (operation == '/') return operand1 / operand2;
+    if (operation == '+') return operand1 + operand2;
+    else if (operation == '-') return operand1 - operand2;
+    else if (operation == '*') return operand1 * operand2;
+    else if (operation == '/')
+    {
+        if ( operand2 > 0)
+            return operand1 / operand2;
+        else{
+            std::cout << " divide by zero error... " << std::endl;
+            return -1;
+        }
+    }
+    else if (operation == '%')
+    {
+        if ( operand2 != 0)
+             return (static_cast<int>(operand1) % static_cast<int>(operand2));
+        else{
+            std::cout <<"arrived" << std::endl;
 
-	else std::cout << "Unexpected Error \n";
-	return -1;
+            std::cout << " divide by zero error... " << std::endl;
+            return -1;
+        }
+    }
+    else std::cout << "Unexpected Error \n";
+    return -1;
 }
 
-// Function to verify whether a character is numeric digit. 
+
+//******************************************************************
+//                         bool isNum(char)
+// pre: receives a character from the expression
+// pos: returns either true or false
+// The function takes in a character and determines whether it is a
+// number or not.
+//*******************************************************************
+// Function to verify whether a character is numeric digit.
 bool Calculator::isNum(char c)
 {
-	if (c >= '0' && c <= '9') return true;
-	return false;
+    if (c >= '0' && c <= '9') return true;
+    return false;
 }
 
+
+
+
+//******************************************************************
+//          double evalPreFix(std::String)
+// pre: receives the user input expression in prefix notation
+// pos: returns a double value  of the expression
+// this function evaluates the prefix notated expression and returns the
+// result back into the main
+// *******************************************************************
+//double Calculator::evalPreFix(std::string prefix) {
+//    int l = prefix.length();
+//    StackADT<int> intobj;
+//    StackADT<char> charObj;
+//    for (int i = l; i > 0; i--) {
+//        if (isdigit(prefix[i]))
+//            intobj.push(prefix[i]);
+//        else{
+//            if(isOperator(prefix[i]))
+//        }
+//            }
+//    return 0;
+//}
+
+
+//******************************************************************
+//             double evaluatePrefix(std::String)
+// pre: receives the prefix notated epxression
+// pos: returns a double value
+// this function takes in the prefix notated expression, evaluates
+// it and returns to main.
+//*******************************************************************
+double Calculator::evaluatePrefix(std::string expression){
+
+    StackADT<double> ob; // Stack object
+    for (int j = expression.size() - 1; j >= 0; j--) {
+        // Push operand to stack
+        // To convert expression[j] to digit subtract
+        // '0' from expression[j].
+        if (isdigit(expression[j]))
+            ob.push(expression[j] - '0');
+        else {
+            // finds operator
+            // and pops from object
+            double exp1 = ob.peek();
+            ob.pop();
+            double exp2 = ob.peek();
+            ob.pop();
+            // Use switch case to operate on exp1
+            // and exp2 and perform exp1 O exp2.
+            switch (expression[j]) {
+            case '+':
+                ob.push(exp1 + exp2);
+                break;
+            case '-':
+                ob.push(exp1 - exp2);
+                break;
+            case '*':
+                ob.push(exp1 * exp2);
+                break;
+            case '/':
+                if(exp2 > 0)
+                    ob.push(exp1 / exp2);
+                else
+                    std:: cout << "divide by zero error" << std::endl;
+                break;
+            case '%':
+                if(exp2 > 0)
+                    ob.push(static_cast<int>(exp1) % static_cast<int>(exp2));
+                else
+                    std::cout << "divide by zero error" << std::endl;
+                break;
+            }
+        }
+    }
+    return ob.peek();        // Returns result of prefix expression
+}
--- main.cpp
+++ main.cpp
@@ -1,179 +1,76 @@
-#if 0
-
-double evaluatePrefix(string expression)
-
-{
-
-	StackADT<double> ob; // Stack object
-
-
-
-	for (int j = expression.size() - 1; j >= 0; j--) {
-
-
-
-		// Push operand to stack
-
-		// To convert expression[j] to digit subtract 
-
-		// '0' from expression[j]. 
-
-		if (isdigit(expression[j]))
-
-			ob.push(expression[j] - '0');
-
-
-
-		else {
-
-
-
-			// finds operator
-
-			// and pops from object
-
-			double exp1 = ob.peek();
-
-			ob.pop();
-
-			double exp2 = ob.peek();
-
-			ob.pop();
-
-
-
-			// Use switch case to operate on exp1 
-
-			// and exp2 and perform exp1 O exp2. 
-
-			switch (expression[j]) {
-
-			case '+':
-
-				ob.push(exp1 + exp2);
-
-				break;
-
-			case '-':
-
-				ob.push(exp1 - exp2);
-
-				break;
-
-			case '*':
-
-				ob.push(exp1 * exp2);
-
-				break;
-
-			case '/':
-
-				ob.push(exp1 / exp2);
-
-				break;
-
-			case '%':
-
-				// ob.push(exp1 % exp2);				TODO
-
-				break;
-
-			}
-
-		}
-
-	}
-
-
-
-	return ob.peek();		// Returns result of prefix expression
-
-}
-
-#endif
-
-
-
 /*
-	Renan Silva Hiramatsu
-	Amber Low
+    Renan Silva Hiramatsu
+    Amber Low
  this lab demonstrates the conversion of postfix and prefix notation
  from a user input and evaluates the expression
 
  do
-	ask user input expression
-	convert expression to postfix notation and evaluate
-	display
-	convert expression to prefix notation and evaluate
-	display
-	ask user if they want to continue
+    ask user input expression
+    convert expression to postfix notation and evaluate
+    display
+    convert expression to prefix notation and evaluate
+    display
+    ask user if they want to continue
  while(user wants to continue)
 
-	infix (2 + 30) / 10 * 2
-	postfix 2 30 + 10 / 2 *
+    infix (2 + 30) / 10 * 2
+    postfix 2 30 + 10 / 2 *
 
  */
 
 #include <iostream>
 #include <string>
 #include "calculator.h"
-
- //    TODO: EXCEPTION OF DIVIDING BY 0
-
 using namespace std;
 
-
-
-#include<iostream>
-#include<string>
-
-using namespace std;
 int main() {
 
-	char input;
-	Calculator ob;
-	string userInput;
-	bool cont = true;
-	string postfix, prefix;
-	do
-	{
-		try {
-			cout << "Enter a mathematical expression: ";
-			getline(cin, userInput);
+    char input;
+    Calculator ob;
+    string userInput;
+    bool cont = true;
+    string postfix, prefix;
+    do
+    {
+        try {
+            cout << "Enter a mathematical expression: ";
+            getline(cin, userInput);
 
 
-			int l = userInput.length();
-			for (int i = 0; i < l; i++) {			// negative values are considered illegal
-				if (userInput[i] == '~')			// if encountered, throw an exception
-					throw - 1;
-			}
-			cout << "Your expression in postfix notation: " << endl;
-			postfix = ob.infixToPostfix(userInput);
-			cout << postfix << endl;
-			cout << "Result: " << ob.evalPostFix(postfix) << endl;
+            int l = userInput.length();
+            for (int i = 0; i < l; i++) {            // negative values are considered illegal
+                if (userInput[i] == '~')            // if encountered, throw an exception
+                    throw - 1;
+            }
+            cout << "Your expression in postfix notation: " << endl;
+            postfix = ob.infixToPostfix(userInput);
+            cout << postfix << endl;
+            cout << "Result: " << ob.evalPostFix(postfix) << endl;
 
-			cout << "Your expression in prefix notation: " << endl;
-			//prefix = ob.infixToPrefix(userInput);
-			//cout << prefix << endl;
+            cout << "Your expression in prefix notation: " << endl;
+            prefix = ob.infixToPrefix(userInput);
+            cout << prefix << endl;
 
-
-			cout << "press 'q' to QUIT and any other letter/number to CONTINUE: ";
-			cin >> input;
-			cin.ignore();
-
-			if (tolower(input) == 'q')
-				cont = false;
-		}
-		catch (int) {
-			cout << "negatative inputs are invalid" << endl;		// catches exception and displays a message
-			cout << "please try again" << endl;						// allows user to try again
-		}
-	} while (cont != false);
+            //cout << "result: " << ob.evaluatePrefix("*9 1") << endl;
 
 
+            cout << "press 'q' to QUIT and any other letter/number to CONTINUE: ";
+            cin >> input;
+            cin.ignore();
+
+            if (tolower(input) == 'q')
+                cont = false;
+        }
+        catch (int) {
+            cout << "negatative inputs are invalid" << endl;        // catches exception and displays a message
+            cout << "please try again" << endl;                        // allows user to try again
+        }
+    } while (cont != false);
 
 
 
-	system("pause");
+
+
+    system("pause");
 
 }
